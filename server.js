a310db18939ab8bf425a499009e289f88516dca8
Danielle Pollesche <dani@kcsburningaromas.com>
2020-07-12 10:54:06 -0700

    updated

--- server.js
+++ server.js
@@ -57,6 +57,9 @@ app.use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+    app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
 
 
